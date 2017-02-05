59c56c1c026981920a382280cd8a2fb966961259
tkrworks <info@tkrworks.net>
2017-02-05 10:55:17 +0900

    デフォのOSC送信先IPを192.168.1.255にする。

--- app/src/main/java/com/jins_meme/bridge/MenuFragment.java
+++ app/src/main/java/com/jins_meme/bridge/MenuFragment.java
@@ -58,8 +58,7 @@ public class MenuFragment extends Fragment implements IResultListener, MemeRealt
 
         // Initialize OSC
         memeOSC = new MemeOSC();
-        //memeOSC.setRemoteIP("192.168.1.255");
-        memeOSC.setRemoteIP("172.24.133.255");// JINS Hackason Guest Wi-Fi Broadcast
+        memeOSC.setRemoteIP("192.168.1.255");
         memeOSC.setRemotePort(10316);
         memeOSC.setHostPort(11316);
         memeOSC.initSocket();
