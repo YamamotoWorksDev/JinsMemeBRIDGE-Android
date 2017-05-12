e8098265de777d7694976632afa87a5cdb544c9e
tkrworks <info@tkrworks.net>
2017-05-12 11:07:18 +0900

    MEMEのスキャン、接続処理とその為のUIを実装。

--- app/src/main/java/com/jins_meme/bridge/BasicConfigFragment.java
+++ app/src/main/java/com/jins_meme/bridge/BasicConfigFragment.java
@@ -2,10 +2,18 @@ package com.jins_meme.bridge;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
 
 /**
  *
@@ -19,9 +27,19 @@ import android.view.ViewGroup;
  **/
 
 public class BasicConfigFragment extends Fragment {
+  Handler handler;
+
+  Switch swScan;
+  Switch swConnect;
+  Spinner spMemeList;
+
+  ArrayAdapter<String> adapter;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    handler = new Handler();
   }
 
   @Override
@@ -37,6 +55,9 @@ public class BasicConfigFragment extends Fragment {
   @Override
   public void onDetach() {
     super.onDetach();
+
+    swScan = null;
+    swConnect = null;
   }
 
   @Override
@@ -45,5 +66,63 @@ public class BasicConfigFragment extends Fragment {
 
     ((MainActivity)getActivity()).setActionBarTitle(getString(R.string.basic_conf) + " SETTING");
     ((MainActivity)getActivity()).setActionBarBack(true);
+
+    swScan = (Switch)view.findViewById(R.id.scan);
+    swScan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if(b) {
+          Log.d("BASIC", "SCAN Start");
+          Toast.makeText(getActivity(), "SCANNING...", Toast.LENGTH_SHORT).show();
+
+          ((MainActivity)getActivity()).startScan();
+
+          adapter.clear();
+          //adapter.addAll(((MainActivity)getActivity()).getScannedMemeList());
+          spMemeList.setAdapter(adapter);
+
+          handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+              swScan.setChecked(false);
+
+              ((MainActivity)getActivity()).stopScan();
+
+              adapter.addAll(((MainActivity)getActivity()).getScannedMemeList());
+            }
+          }, 5000);
+        }
+        else {
+          Log.d("BASIC", "SCAN Stop");
+          Toast.makeText(getActivity(), "SCAN STOPPED.", Toast.LENGTH_SHORT).show();
+
+          ((MainActivity)getActivity()).stopScan();
+        }
+      }
+    });
+
+    swConnect = (Switch)view.findViewById(R.id.connect);
+    swConnect.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if(b) {
+          String memeId = (String)spMemeList.getSelectedItem();
+          Log.d("BASIC", "CONNECT Start " + memeId);
+          Toast.makeText(getActivity(), "CONNECTING TO " + memeId + "...", Toast.LENGTH_SHORT).show();
+
+          ((MainActivity)getActivity()).connectToMeme(memeId);
+        }
+        else {
+          Log.d("BASIC", "CONNECT Stop");
+          Toast.makeText(getActivity(), "DISCONNECTING...", Toast.LENGTH_SHORT).show();
+
+          ((MainActivity)getActivity()).disconnectToMeme();
+        }
+      }
+    });
+
+    spMemeList = (Spinner)view.findViewById(R.id.meme_list);
+    adapter = new ArrayAdapter<String>(getContext( ), android.R.layout.simple_spinner_item);
+    spMemeList.setAdapter(adapter);
   }
 }
