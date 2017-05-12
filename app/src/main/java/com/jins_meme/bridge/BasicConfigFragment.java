2a080b4e8a8a9af396c888f9d35a0eb5260df337
tkrworks <info@tkrworks.net>
2017-05-13 01:14:41 +0900

    MEMEのスキャン情報、接続状態の保持、APP_ID、APP_SECRETの値を保持する。

--- app/src/main/java/com/jins_meme/bridge/BasicConfigFragment.java
+++ app/src/main/java/com/jins_meme/bridge/BasicConfigFragment.java
@@ -3,16 +3,21 @@ package com.jins_meme.bridge;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -27,13 +32,17 @@ import android.widget.Toast;
  **/
 
 public class BasicConfigFragment extends Fragment {
-  Handler handler;
+  private Handler handler;
 
-  Switch swScan;
-  Switch swConnect;
-  Spinner spMemeList;
+  private Switch swScan;
+  private Switch swConnect;
+  private Spinner spMemeList;
+  private EditText etAppId;
+  private EditText etAppSecret;
 
-  ArrayAdapter<String> adapter;
+  private ArrayAdapter<String> adapter;
+
+  private String selectedMemeID;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +53,8 @@ public class BasicConfigFragment extends Fragment {
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    Log.d("BASIC", "onCreateView");
+
     return inflater.inflate(R.layout.fragment_basicconfig, container, false);
   }
 
@@ -56,18 +67,25 @@ public class BasicConfigFragment extends Fragment {
   public void onDetach() {
     super.onDetach();
 
+    Log.d("BASIC", "onDetach");
+
     swScan = null;
     swConnect = null;
+    spMemeList = null;
+    etAppId = null;
+    etAppSecret = null;
   }
 
   @Override
   public void onViewCreated(View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
 
+    Log.d("BASIC", "onViewCreated");
+
     ((MainActivity)getActivity()).setActionBarTitle(getString(R.string.basic_conf) + " SETTING");
     ((MainActivity)getActivity()).setActionBarBack(true);
 
-    swScan = (Switch)view.findViewById(R.id.scan);
+    swScan = (Switch) view.findViewById(R.id.scan);
     swScan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
       @Override
       public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
@@ -78,8 +96,7 @@ public class BasicConfigFragment extends Fragment {
           ((MainActivity)getActivity()).startScan();
 
           adapter.clear();
-          //adapter.addAll(((MainActivity)getActivity()).getScannedMemeList());
-          spMemeList.setAdapter(adapter);
+          //spMemeList.setAdapter(adapter);
 
           handler.postDelayed(new Runnable() {
             @Override
@@ -88,7 +105,11 @@ public class BasicConfigFragment extends Fragment {
 
               ((MainActivity)getActivity()).stopScan();
 
-              adapter.addAll(((MainActivity)getActivity()).getScannedMemeList());
+              if(((MainActivity)getActivity()).getScannedMemeSize() > 0) {
+                adapter.addAll(((MainActivity)getActivity()).getScannedMemeList());
+                swConnect.setEnabled(true);
+                selectedMemeID = (String)spMemeList.getSelectedItem();
+              }
             }
           }, 5000);
         }
@@ -101,16 +122,18 @@ public class BasicConfigFragment extends Fragment {
       }
     });
 
-    swConnect = (Switch)view.findViewById(R.id.connect);
+    swConnect = (Switch) view.findViewById(R.id.connect);
     swConnect.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
       @Override
       public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
         if(b) {
-          String memeId = (String)spMemeList.getSelectedItem();
-          Log.d("BASIC", "CONNECT Start " + memeId);
-          Toast.makeText(getActivity(), "CONNECTING TO " + memeId + "...", Toast.LENGTH_SHORT).show();
+          if(!((MainActivity)getActivity()).isMemeConnected()) {
+            selectedMemeID = (String)spMemeList.getSelectedItem();
+            Log.d("BASIC", "CONNECT Start " + selectedMemeID);
+            Toast.makeText(getActivity(), "CONNECTING TO " + selectedMemeID + "...", Toast.LENGTH_SHORT).show();
 
-          ((MainActivity)getActivity()).connectToMeme(memeId);
+            ((MainActivity)getActivity()).connectToMeme(selectedMemeID);
+          }
         }
         else {
           Log.d("BASIC", "CONNECT Stop");
@@ -120,9 +143,75 @@ public class BasicConfigFragment extends Fragment {
         }
       }
     });
+    if(((MainActivity)getActivity()).isMemeConnected() || ((MainActivity)getActivity()).getScannedMemeSize() > 0) {
+      swConnect.setEnabled(true);
+    }
+    else {
+      swConnect.setEnabled(false);
+    }
+
+    Log.d("BASIC", "spMemeList new");
 
     spMemeList = (Spinner)view.findViewById(R.id.meme_list);
-    adapter = new ArrayAdapter<String>(getContext( ), android.R.layout.simple_spinner_item);
+    adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item);
+    if(((MainActivity)getActivity()).getScannedMemeSize() > 0) {
+      adapter.addAll(((MainActivity)getActivity()).getScannedMemeList());
+      setSelection(selectedMemeID);
+    }
     spMemeList.setAdapter(adapter);
+
+    etAppId = (EditText) view.findViewById(R.id.app_id);
+    etAppId.addTextChangedListener(new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+      }
+
+      @Override
+      public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        ((MainActivity)getActivity()).autoSaveText("APP_ID", charSequence.toString());
+        Log.d("BASIC", "APP_ID: " + charSequence.toString());
+      }
+
+      @Override
+      public void afterTextChanged(Editable editable) {
+
+      }
+    });
+    etAppId.setText(((MainActivity)getActivity()).getSavedText("APP_ID"), TextView.BufferType.EDITABLE);
+
+    etAppSecret = (EditText) view.findViewById(R.id.app_secret);
+    etAppSecret.addTextChangedListener(new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+      }
+
+      @Override
+      public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        ((MainActivity)getActivity()).autoSaveText("APP_SECRET", charSequence.toString());
+        Log.d("BASIC", "APP_SECRET: " + charSequence.toString());
+      }
+
+      @Override
+      public void afterTextChanged(Editable editable) {
+
+      }
+    });
+    etAppSecret.setText(((MainActivity)getActivity()).getSavedText("APP_SECRET"), TextView.BufferType.EDITABLE);
+
+    Log.d("BASIC", "adapter count = " + adapter.getCount());
+
+  }
+
+  private void setSelection(@NonNull String item) {
+    int index = 0;
+    for(int i = 0; i < adapter.getCount(); i++) {
+      if(adapter.getItem(i).equals(item)) {
+        index = i;
+        break;
+      }
+    }
+    spMemeList.setSelection(index);
   }
 }
