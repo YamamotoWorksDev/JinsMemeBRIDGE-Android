39670293fd300d48c154e2fc0bcfe7ba5f9b5870
tkrworks <info@tkrworks.net>
2017-05-17 14:47:37 +0900

    OSCの受信テストが行えるようにする。 ポート番号を空にしたときに値を保存しないようにする。 ポート番号が空の状態でMenuFragmentに戻ったときにアプリが落ちないよう修正。

--- app/src/main/java/com/jins_meme/bridge/OSCConfigFragment.java
+++ app/src/main/java/com/jins_meme/bridge/OSCConfigFragment.java
@@ -11,6 +11,7 @@ package com.jins_meme.bridge;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.InputFilter;
@@ -28,22 +29,21 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.Collections;
-import java.util.List;
+import android.widget.Toast;
 
 public class OSCConfigFragment extends Fragment {
 
+  private Handler handler;
   private RelativeLayout layout;
   private EditText etRemoteIP;
   private EditText etRemotePort;
   private EditText etHostIP;
   private EditText etHostPort;
   private Button btnTest;
+  private static Toast toast;
 
   private MemeOSC testOSC;
+  private boolean isShown = true;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -66,6 +66,8 @@ public class OSCConfigFragment extends Fragment {
   public void onDetach() {
     super.onDetach();
 
+    handler = null;
+    layout = null;
     etRemoteIP = null;
     etRemotePort = null;
     etHostIP = null;
@@ -76,11 +78,15 @@ public class OSCConfigFragment extends Fragment {
       testOSC.closeSocket();
       testOSC = null;
     }
+
+    isShown = false;
   }
 
   @Override
   public void onViewCreated(View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+
+    handler = new Handler();
 
     layout = (RelativeLayout) view.findViewById(R.id.osc_layout);
     layout.setOnTouchListener(new OnTouchListener() {
@@ -189,10 +195,13 @@ public class OSCConfigFragment extends Fragment {
       public void afterTextChanged(Editable editable) {
         Log.d("DEBUG", "after text changed " + editable.toString());
 
-        ((MainActivity) getActivity()).autoSaveValue("REMOTE_PORT", Integer.valueOf(editable.toString()));
+        if(editable.toString().length() > 0) {
+          ((MainActivity) getActivity())
+              .autoSaveValue("REMOTE_PORT", Integer.valueOf(editable.toString()));
 
-        testOSC.setRemotePort(Integer.parseInt(etRemotePort.getText().toString()));
-        testOSC.initSocket();
+          testOSC.setRemotePort(Integer.parseInt(editable.toString()));
+          testOSC.initSocket();
+        }
       }
     });
 
@@ -226,10 +235,11 @@ public class OSCConfigFragment extends Fragment {
       public void afterTextChanged(Editable editable) {
         Log.d("DEBUG", "after text changed " + editable.toString());
 
-        ((MainActivity) getActivity()).autoSaveValue("HOST_PORT", Integer.valueOf(editable.toString()));
-
-        testOSC.setHostPort(Integer.parseInt(etHostPort.getText().toString()));
-        testOSC.initSocket();
+        if(editable.toString().length() > 0) {
+          ((MainActivity) getActivity()).autoSaveValue("HOST_PORT", Integer.valueOf(editable.toString()));
+          testOSC.setHostPort(Integer.parseInt(editable.toString()));
+          testOSC.initSocket();
+        }
       }
     });
 
@@ -250,5 +260,91 @@ public class OSCConfigFragment extends Fragment {
     testOSC.setRemotePort(Integer.parseInt(etRemotePort.getText().toString()));
     testOSC.setHostPort(Integer.parseInt(etHostPort.getText().toString()));
     testOSC.initSocket();
+
+    isShown = true;
+    Thread rcvTestThread = new Thread(new ReceiveTestTRunnable());
+    rcvTestThread.start();
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+
+    Log.d("DEBUG", "destroy view...");
+
+    isShown = false;
+
+    handler = null;
+    layout = null;
+    etRemoteIP = null;
+    etRemotePort = null;
+    etHostIP = null;
+    etHostPort = null;
+    btnTest = null;
+
+    if(testOSC != null) {
+      testOSC.closeSocket();
+      testOSC = null;
+    }
+  }
+
+  class ReceiveTestTRunnable implements Runnable {
+    @Override
+    public void run() {
+      while (isShown) {
+        //Log.d("DEBUG", "receive polling...");
+
+        try {
+          testOSC.receiveMessage();
+          testOSC.extractAddressFromPacket();
+          testOSC.extractTypeTagFromPacket();
+          testOSC.extractArgumentsFromPacket();
+
+          //Log.d("DEBUG", String.format("%s %s %d", testOSC.getAddress(), testOSC.getTypeTags(), testOSC.getArgumentsLength()));
+
+          String rcvMessage = testOSC.getAddress();
+          int argslen = testOSC.getArgumentsLength();
+          String typetags = testOSC.getTypeTags();
+          for (int i = 0; i < argslen; i++) {
+            switch (typetags.substring(i, i + 1)) {
+              case "i":
+                rcvMessage += " " + testOSC.getIntArgumentAtIndex(i);
+                break;
+              case "f":
+                rcvMessage += " " + testOSC.getFloatArgumentAtIndex(i);
+                break;
+              case "s":
+                rcvMessage += " " + testOSC.getStringArgumentAtIndex(i);
+                break;
+              default:
+                break;
+            }
+          }
+          final String rm = rcvMessage;
+
+          handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+              if (toast != null) {
+                toast.cancel();
+              }
+              if(rm != null && rm.length() > 0) {
+                toast = Toast.makeText(getActivity(), rm, Toast.LENGTH_SHORT);
+                toast.show();
+              }
+            }
+          });
+        } catch (NullPointerException e) {
+
+        }
+
+        try {
+          Thread.sleep(100);
+        } catch(InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    }
   }
 }
