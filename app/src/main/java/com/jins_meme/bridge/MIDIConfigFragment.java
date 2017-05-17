8f1072342c6e4a901c8351ec18b3fe3898c8b15f
tkrworks <info@tkrworks.net>
2017-05-17 17:49:37 +0900

    MIDIチャンネルを設定できるようにする。 MIDIの送信テストを行えるようにする。

--- app/src/main/java/com/jins_meme/bridge/MIDIConfigFragment.java
+++ app/src/main/java/com/jins_meme/bridge/MIDIConfigFragment.java
@@ -12,14 +12,36 @@ package com.jins_meme.bridge;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Spinner;
 
 public class MIDIConfigFragment extends Fragment {
+
+  private Spinner spMidiCh;
+  private SeekBar sbCC72;
+  private SeekBar sbCC73;
+  private SeekBar sbCC74;
+  private SeekBar sbCC75;
+  private Button btnNote72;
+  private Button btnNote73;
+  private Button btnNote74;
+  private Button btnNote75;
+
+  MemeMIDI testMIDI;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -42,6 +64,19 @@ public class MIDIConfigFragment extends Fragment {
   @Override
   public void onDetach() {
     super.onDetach();
+
+    spMidiCh = null;
+    sbCC72 = null;
+    sbCC73 = null;
+    sbCC74 = null;
+    sbCC75 = null;
+    btnNote72 = null;
+    btnNote73 = null;
+    btnNote74 = null;
+    btnNote75 = null;
+
+    testMIDI.closePort();
+    testMIDI = null;
   }
 
   @Override
@@ -61,7 +96,7 @@ public class MIDIConfigFragment extends Fragment {
         public void onClick(DialogInterface dialogInterface, int i) {
           Log.d("DEBUG", "Quit App...");
 
-          ((MainActivity) getActivity()).finish();
+          getActivity().finish();
         }
       });
       alert.setNegativeButton("OK", new DialogInterface.OnClickListener() {
@@ -76,5 +111,190 @@ public class MIDIConfigFragment extends Fragment {
 
       alert.create().show();
     }
+
+    spMidiCh = (Spinner) view.findViewById(R.id.midi_ch);
+    spMidiCh.setSelection(((MainActivity) getActivity()).getSavedValue("MIDI_CH", 0));
+    spMidiCh.setOnItemSelectedListener(new OnItemSelectedListener() {
+      @Override
+      public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Log.d("DEBUG", "position = " + i);
+
+        ((MainActivity) getActivity()).autoSaveValue("MIDI_CH", i);
+      }
+
+      @Override
+      public void onNothingSelected(AdapterView<?> adapterView) {
+
+      }
+    });
+
+    sbCC72 = (SeekBar) view.findViewById(R.id.cc_72);
+    sbCC72.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        testMIDI.sendControlChange(spMidiCh.getSelectedItemPosition() + 1, 72, i);
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+
+      }
+    });
+
+    sbCC73 = (SeekBar) view.findViewById(R.id.cc_73);
+    sbCC73.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        testMIDI.sendControlChange(spMidiCh.getSelectedItemPosition() + 1, 73, i);
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+
+      }
+    });
+
+    sbCC74 = (SeekBar) view.findViewById(R.id.cc_74);
+    sbCC74.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        testMIDI.sendControlChange(spMidiCh.getSelectedItemPosition() + 1, 74, i);
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+
+      }
+    });
+
+    sbCC75 = (SeekBar) view.findViewById(R.id.cc_75);
+    sbCC75.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        testMIDI.sendControlChange(spMidiCh.getSelectedItemPosition() + 1, 75, i);
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+
+      }
+    });
+
+    btnNote72 = (Button) view.findViewById(R.id.note_72);
+    btnNote72.setOnTouchListener(new OnTouchListener() {
+      @Override
+      public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+          Log.d("DEBUG", "72 down");
+
+          testMIDI.sendNote(spMidiCh.getSelectedItemPosition() + 1, 72, 127);
+        }
+        else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+          Log.d("DEBUG", "72 up");
+
+          testMIDI.sendNote(spMidiCh.getSelectedItemPosition() + 1, 72, 0);
+        }
+
+        return false;
+      }
+    });
+
+    btnNote73 = (Button) view.findViewById(R.id.note_73);
+    btnNote73.setOnTouchListener(new OnTouchListener() {
+      @Override
+      public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+          Log.d("DEBUG", "73 down");
+
+          testMIDI.sendNote(spMidiCh.getSelectedItemPosition() + 1, 73, 127);
+        }
+        else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+          Log.d("DEBUG", "73 up");
+
+          testMIDI.sendNote(spMidiCh.getSelectedItemPosition() + 1, 73, 0);
+        }
+
+        return false;
+      }
+    });
+
+    btnNote74 = (Button) view.findViewById(R.id.note_74);
+    btnNote74.setOnTouchListener(new OnTouchListener() {
+      @Override
+      public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+          Log.d("DEBUG", "74 down");
+
+          testMIDI.sendNote(spMidiCh.getSelectedItemPosition() + 1, 74, 127);
+        }
+        else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+          Log.d("DEBUG", "74 up");
+
+          testMIDI.sendNote(spMidiCh.getSelectedItemPosition() + 1, 74, 0);
+        }
+
+        return false;
+      }
+    });
+
+    btnNote75 = (Button) view.findViewById(R.id.note_75);
+    btnNote75.setOnTouchListener(new OnTouchListener() {
+      @Override
+      public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+          Log.d("DEBUG", "75 down");
+
+          testMIDI.sendNote(spMidiCh.getSelectedItemPosition() + 1, 75, 127);
+        }
+        else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+          Log.d("DEBUG", "75 up");
+
+          testMIDI.sendNote(spMidiCh.getSelectedItemPosition() + 1, 75, 0);
+        }
+
+        return false;
+      }
+    });
+
+    testMIDI = new MemeMIDI(getContext());
+    testMIDI.initPort();
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+
+    spMidiCh = null;
+    sbCC72 = null;
+    sbCC73 = null;
+    sbCC74 = null;
+    sbCC75 = null;
+    btnNote72 = null;
+    btnNote73 = null;
+    btnNote74 = null;
+    btnNote75 = null;
+
+    testMIDI.closePort();
+    testMIDI = null;
   }
 }
