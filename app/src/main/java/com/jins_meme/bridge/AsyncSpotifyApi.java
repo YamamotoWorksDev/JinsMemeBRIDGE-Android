3dff1c19d5a3e66c80433e151c326549694314a6
tkrworks <info@tkrworks.net>
2017-06-14 12:06:09 +0900

    Featured Playlistの参照、アーティスト検索、アルバム検索に対応。

--- app/src/main/java/com/jins_meme/bridge/AsyncSpotifyApi.java
+++ app/src/main/java/com/jins_meme/bridge/AsyncSpotifyApi.java
@@ -5,7 +5,13 @@ import android.util.Log;
 import java.util.List;
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Album;
+import kaaes.spotify.webapi.android.models.AlbumSimple;
+import kaaes.spotify.webapi.android.models.AlbumsPager;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.Artists;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
+import kaaes.spotify.webapi.android.models.FeaturedPlaylists;
 import kaaes.spotify.webapi.android.models.Pager;
 import kaaes.spotify.webapi.android.models.PlaylistSimple;
 
@@ -37,16 +43,39 @@ public class AsyncSpotifyApi extends AsyncTask<String, String, String> {
         Log.d("DEBUG", "ASYNC SPOTIFY:: Country -> " + country);
         break;
       case "user_playlist":
-        Pager<PlaylistSimple> pager = mSpotifyService.getMyPlaylists();
-        Log.d("DEBUG", "ASYNC SPOTIFY:: My Playlist -> " + pager.total);
-        for (int i = 0; i < pager.total; i++) {
-          List<PlaylistSimple> list = pager.items;
-          Log.d("DEBUG", "ASYNC SPOTIFY::   --> " + list.get(0).name + " " + list.get(0).uri);
+        Pager<PlaylistSimple> upPager = mSpotifyService.getMyPlaylists();
+        Log.d("DEBUG", "ASYNC SPOTIFY:: My Playlist -> " + upPager.total);
+        for (int i = 0; i < upPager.total; i++) {
+          List<PlaylistSimple> list = upPager.items;
+          Log.d("DEBUG", "ASYNC SPOTIFY::   --> " + list.get(i).name + " " + list.get(i).uri);
+        }
+        break;
+      case "featured_playlist":
+        FeaturedPlaylists featuredPlaylists = mSpotifyService.getFeaturedPlaylists();
+        Pager<PlaylistSimple> fpPager = featuredPlaylists.playlists;
+        Log.d("DEBUG", "ASYNC SPOTIFY:: Featured Playlist -> " + fpPager.total);
+        for (int i = 0; i < fpPager.total; i++) {
+          List<PlaylistSimple> list = fpPager.items;
+          Log.d("DEBUG", "ASYNC SPOTIFY::   --> " + list.get(i).name + " " + list.get(i).uri);
         }
         break;
       case "search_artist":
-        ArtistsPager artistsPager = mSpotifyService.searchArtists("tokimonsta");
-        Log.d("DEBUG", "ASYNC SPOTIFY:: Artists -> " + artistsPager.toString());
+        ArtistsPager artistsPager = mSpotifyService.searchArtists(strings[1]);
+        Pager<Artist> aPager =  artistsPager.artists;
+        Log.d("DEBUG", "ASYNC SPOTIFY:: Searched Artist -> " + strings[1] + " " + aPager.total);
+        for (int i = 0; i < aPager.total; i++) {
+          List<Artist> list = aPager.items;
+          Log.d("DEBUG", "ASYNC SPOTIFY::   --> " + list.get(i).name + " " + list.get(i).uri);
+        }
+        break;
+      case "search_album":
+        AlbumsPager albumsPager = mSpotifyService.searchAlbums(strings[1]);
+        Pager<AlbumSimple> albPager =  albumsPager.albums;
+        Log.d("DEBUG", "ASYNC SPOTIFY:: Searched Album -> " + albPager.total);
+        for (int i = 0; i < albPager.total; i++) {
+          List<AlbumSimple> list = albPager.items;
+          Log.d("DEBUG", "ASYNC SPOTIFY::   --> " + list.get(i).name + " " + list.get(i).uri);
+        }
         break;
     }
 
