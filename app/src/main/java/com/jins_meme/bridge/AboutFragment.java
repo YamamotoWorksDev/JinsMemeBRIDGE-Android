25adee1efa017d77b42fecd0aa6e5ef69de1cd95
tkrworks <info@tkrworks.net>
2017-07-18 13:56:15 +0900

    いい加減、build.gradleのversionName変えたら、このアプリについて（About）のバージョン番号が変わるようにする。

--- app/src/main/java/com/jins_meme/bridge/AboutFragment.java
+++ app/src/main/java/com/jins_meme/bridge/AboutFragment.java
@@ -10,11 +10,14 @@
 package com.jins_meme.bridge;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AboutFragment extends ConfigFragmentBase {
 
@@ -49,5 +52,17 @@ public class AboutFragment extends ConfigFragmentBase {
   @Override
   public void onViewCreated(View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+
+    PackageManager packageManager = getActivity().getPackageManager();
+    String versionName = "0.0.0";
+    try {
+      PackageInfo packageInfo = packageManager.getPackageInfo(getActivity().getPackageName(), 0);
+      versionName = packageInfo.versionName;
+    } catch (NameNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    TextView versionNameView = (TextView) view.findViewById(R.id.version_name);
+    versionNameView.setText(versionName);
   }
 }
